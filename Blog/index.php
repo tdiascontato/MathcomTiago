ca3ae08258542d7d271aa4a404c927a68428457a
Tiago Dias <98658691+tdiascontato@users.noreply.github.com>
2023-05-31 10:01:01 -0300

    Add files via upload

---
+++ Blog/index.php
@@ -0,0 +1,21 @@
+<?php
+    include_once("helpers/url.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Math com Tiago</title>
+    <!-- Colocar o link certo -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
+    <!-- Google Fonts> -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <h1>Olá, mundo! Será que vamos?</h1>
+</body>
+</html>
