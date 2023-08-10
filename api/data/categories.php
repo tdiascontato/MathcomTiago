28e2d011bf16bae92e3b88f3ddb4ca8f6b775f8a
Tiago Dias <98658691+tdiascontato@users.noreply.github.com>
2023-08-10 10:43:28 -0300

    Add files via upload

---
+++ api/data/categories.php
@@ -0,0 +1,11 @@
+<?php
+    $categories = [
+        'Operacoes Numericas',
+        'Operacoes com fracoes',
+        'Potencias',
+        'Radiciacao',
+        'Regra de 3',
+        'Momomios',
+        'Polinomios'
+    ];
+?>
---
+++ api/index.php
@@ -0,0 +1,3 @@
+<?php
+phpinfo();
+include_once __DIR__ . "/home.php";
